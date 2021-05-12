cb7a24e18574f65f85abd645905d2a186fc4bc25
Tim Garrels <tim.garrels@protonmail.com>
2021-05-12 09:59:13 +0200

    Replace Adj. List with Edge List

--- SpreadSheetGraph.py
+++ SpreadSheetGraph.py
@@ -1,17 +1,19 @@
 """Creates a Graph from Label Regions"""
 import graphviz
-
+from functools import reduce
 
 class SpreadSheetGraph(object):
+    def __init__(self, nodes, edge_list):
+        self.nodes = nodes
+        self.edge_list = edge_list
+
+        self.edge_toggle_list = [True for _ in range(len(self.edge_list))]
+
     def from_label_regions(lr_list):
         """Creates a graph from label regions
         Refer to `A Genetic-based Search for Adaptive TableRecognition in Spreadsheets.pdf`
         for the approach"""
-
-        g = SpreadSheetGraph()
-        g.nodes = lr_list
-
-        adj_list = [[] for _ in range(len(g.nodes))]
+        edge_list = []
         for i, source in enumerate(lr_list):
             for j, dest in enumerate(lr_list):
                 if i != j:
@@ -25,21 +27,28 @@ class SpreadSheetGraph(object):
                     vertical_overlap = True if len(set(source_ys).intersection(dest_ys)) > 0 else False
 
                     if horizontal_overlap or vertical_overlap:
-                        adj_list[i].append({
-                            "index": j,
+                        edge_list.append({
+                            "source": i,
+                            "dest": j,
                             "overlap_type": "horizontal" if horizontal_overlap else "vertical"
                         })
-        g.adj_list = adj_list
-        return g
+        return SpreadSheetGraph(lr_list, edge_list)
 
     def visualize(self, format="png", engine="dot", out="out"):
+        """Uses graphviz to visualize the current graph"""
         g = graphviz.Graph(format=format, engine=engine, strict=True)
         for i, node in enumerate(self.nodes):
             color = "blue" if node["type"] == "Header" else "green"
             g.node(str(i), color=color)
 
-        for source in range(len(self.adj_list)):
-            for dest in self.adj_list[source]:
-                style = "solid" if dest["overlap_type"] == "horizontal" else "dashed"
-                g.edge(str(source), str(dest["index"]), style=style)
+        all_true = reduce(lambda x,y: x and y, self.edge_toggle_list)
+        all_false = reduce(lambda x,y: not x and not y, self.edge_toggle_list)
+
+        for i, edge in enumerate(self.edge_list):
+            enabled = self.edge_toggle_list[i]
+            style = "solid" if edge["overlap_type"] == "horizontal" else "dashed"
+            color = "green" if enabled else "red"
+            if all_true or all_false:
+                color = "black"
+            g.edge(str(edge["source"]), str(edge["dest"]), style=style, color=color)
         g.render(out)
